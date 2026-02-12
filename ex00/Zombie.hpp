07d521195b0c41c9213722d768993a5c89d52dfa
Rbs <rubsanch@student.42madrid.com>
2026-02-12 06:37:23 +0100

    files move to ex00

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 18:47:24 by rubsanch          #+#    #+#             */
+/*   Updated: 2026/02/11 18:53:04 by rubsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+Public:
+	Zombie* zombieHorde(int N, std::string name);
+	void randomChump(std::string name);
+Private:
+	std::string	_name;
+	void		announce(void);
+}
+
+#endif
