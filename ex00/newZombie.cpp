cedbe4f811bd9ab97d82b20abbf474aeeb7d226f
Rbs <rubsanch@student.42madrid.com>
2026-02-12 11:14:49 +0100

    boolean in use

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 07:17:13 by rubsanch          #+#    #+#             */
-/*   Updated: 2026/02/12 08:52:35 by rubsanch         ###   ########.fr       */
+/*   Updated: 2026/02/12 11:14:32 by rubsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie*	newZombie(std::string name)
 {
 	Zombie	*z;
 
-	if (name.empty() == 1)
+	if (name.empty() == true)
 		return (NULL);
 	z = new Zombie(name);
 	return (z);
