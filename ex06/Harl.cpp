2df762b71a959d41f382d9439e1f2034e4a3843e
Rbs <rubsanch@student.42madrid.com>
2026-02-13 17:15:49 +0100

    done

---
+++ ex06/Harl.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 14:44:21 by rubsanch          #+#    #+#             */
+/*   Updated: 2026/02/13 17:12:08 by rubsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+	std::cout << "[DEBUG] I love having extra bacon for my "
+		<< "7XL-double-cheese-triple-pickle-specialketchup burger. "
+		<< "I really do!"
+		<< std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "[INFO] I cannot believe adding extra bacon costs more money. "
+	   << "You didn’t put enough bacon in my burger! If you did, "
+	   << "I wouldn’t be asking for more!"
+	   << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "[WARNING] "
+		<< "I think I deserve to have some extra bacon for free. "
+		<< "I’ve been coming for years, whereas you started working here "
+		<< "just last month."
+		<< std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "[ERROR] "
+		<< "This is unacceptable! I want to speak to the manager now."
+		<< std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	static const std::string	db[LEVELS_LEN] =
+	{
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+	unsigned int		index;
+
+	index = LEVELS_LEN;
+	for (unsigned int i = 0; i < LEVELS_LEN; i++)
+	{
+		if (db[i] == level)
+		{
+			index = i;
+			break ;
+		}
+	}
+	switch (index)
+	{
+		case 0:
+			this->debug();
+		case 1:
+			this->info();
+		case 2:
+			this->warning();
+		case 3:
+			this->error();
+			break ;
+		default:
+			std::cout << "Level " << level << " is WRONG" << std::endl;
+	};
+}
---
+++ ex06/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 14:44:21 by rubsanch          #+#    #+#             */
+/*   Updated: 2026/02/13 17:14:52 by rubsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(int argc, char *argv[])
+{
+	Harl	h;
+
+	std::cout << "Complaining as WRONG_LEVEL: " << std::endl;
+	h.complain("WRONG_LEVEL");
+	std::cout << std::endl;
+
+	std::cout << "Complaining as DEBUG: " << std::endl;
+	h.complain("DEBUG");
+	std::cout << std::endl;
+
+	std::cout << "Complaining as INFO: " << std::endl;
+	h.complain("INFO");
+	std::cout << std::endl;
+
+	std::cout << "Complaining as WARNING: " << std::endl;
+	h.complain("WARNING");
+	std::cout << std::endl;
+
+	std::cout << "Complaining as ERROR: " << std::endl;
+	h.complain("ERROR");
+	std::cout << std::endl;
+
+	if (argc == 2)
+	{
+		std::cout << "Complaining as " << argv[1] << ": " << std::endl;
+		h.complain(argv[1]);
+		std::cout << std::endl;
+	}
+
+	return (0);
+}
